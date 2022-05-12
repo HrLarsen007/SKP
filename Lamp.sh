6bb19af87d89fd636ce43727d63e59451bc32dd2
Morten Larsen <hrlarsen007@gmail.com>
2022-05-12 10:30:12 +0200

    wordpress test

---
+++ Lamp.sh
@@ -0,0 +1,115 @@
+## opsætning af en webserver på Rocky 8 
+
+
+wp_source="https://wordpress.org/latest.tar.gz"
+server_root="/var/www/html" 
+
+localuser="wpuser"
+remoteuser="hemliguser"
+database="wpdatabase"
+table="wp_"
+
+## Opdater Rocky
+
+sudo yum -y update && yum -y upgrade
+sudo yum -y clean all
+
+
+## Installere nogle brugebar pakker til systemet samt Remi og epel til at bruge til at installere PHP
+
+sudo yum -y install yum-utils
+sudo yum -y install epel-release
+
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-8.rpm
+
+sudo yum -y update && yum -y upgrade
+
+## installing Apache
+sudo yum install -y httpd
+
+
+## Fjerner alt PHP der ligger i forvejen
+sudo yum -y remove php php-fpm
+sudo yum -y remove php*
+sudo yum -y module list reset php
+sudo yum -y module reset php
+
+
+## Enabler php:remi-8.1 repo så vi kan download fra det
+sudo yum -y module enable php:remi-8.1
+
+## Installere PHP via remi repo
+sudo yum -y install php
+
+## Installere alle pakker til PHP (Bedst pratice er kun at installer dem vi skal bruge)
+sudo yum -y install php-cli php-fpm php-curl php-mysqlnd php-gd php-opcache php-zip php-intl php-common php-bcmath php-imap php-imagick php-xmlrpc php-json php-readline php-memcached php-redis php-mbstring php-apcu php-xml php-ldap php-fileinfo php-pdo
+
+sudo yum -y update && yum -y upgrade
+
+## laver en test side til at se vores php indstillinger
+touch /var/www/html/phpinfo.php && echo '<?php phpinfo(); ?>' >> /var/www/html/phpinfo.php 
+
+## Installere database (mariadb)
+sudo yum -y install mariadb-server mariadb
+
+## installere nogle quality of life værktøjer
+sudo yum -y install nano wget curl net-tools git rsync 
+
+## Installere perl relateret pakker, i tilfælde af der skal køres perl scripts.
+sudo yum -y install perl perl-Net-SSLeay unzip perl-Encode-Detect perl-Data-Dumper
+
+## Strater og sætter services til at start op når maskine starter.
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+sudo systemctl start mariadb.service
+sudo systemctl enable mariadb.service
+
+sudo yum -y update && yum -y upgrade
+
+## installere database (mariadb/mysql)
+sudo mysql_secure_installation
+
+## Sætter rettighederene til apache
+sudo chmod 755 /var/www -R
+sudo chown apache:apache $server_root/* -R 
+
+## Firewall regler (kun hvis der skal være adgang udenfor local host)
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https
+sudo firewall-cmd --permanent --zone=public --add-port=3306/tcp ##Database (mariadb)
+sudo firewall-cmd --reload
+
+
+## laver en focus update på selinux-policy
+sudo yum update -y selinux-policy*
+
+## Genstarter systemerne
+sudo systemctl restart httpd.service
+sudo systemctl restart mariadb.service
+wget $wp_source
+tar xpvf latest.tar.gz
+sudo rsync -avP wordpress/ $server_root
+rm -rf latest.tar.gz
+rm -rf wordpress
+
+Q1="CREATE DATABASE $database;"
+Q2="CREATE USER $localuser@'localhost' IDENTIFIED BY '$localpass';"
+Q3="GRANT ALL PRIVILEGES on $database.* TO $localuser@localhost;"
+Q4="FLUSH PRIVILEGES;"
+Q5="CREATE USER $remoteuser@'%' IDENTIFIED BY '$remotepass';"
+Q6="GRANT ALL PRIVILEGES ON $database.* TO $remoteuser@'%' WITH GRANT OPTION;"
+Q7="FLUSH PRIVILEGES;"
+SQL=${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}${Q7}
+
+if `mysql -u root -p -e "$SQL"` ; then
+		echo "DB installeret"
+	else
+		`mysql -u root -p -e "$SQL"`
+fi
+
+sudo cp $server_root/wp-config-sample.php $server_root/wp-config.php
+sudo sed -i "s/database_name_here/$database/g" $server_root/wp-config.php
+sudo sed -i "s/username_here/$localuser/g" $server_root/wp-config.php
+sudo sed -i "s/password_here/$localpass/g" $server_root/wp-config.php
+sudo sed -i "s/wp_/$table/g" $server_root/wp-config.php
